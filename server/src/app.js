e846eab54a21402914815be8139a22cd47cfc487
LuisFOsG <luisfosgdev@gmail.com>
2021-11-27 19:29:59 -0500

    feat(client): connect with server

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
+
 const app = express()
 
-app.set('PORT', process.env.PORT || 3000)
+app.set('PORT', process.env.PORT || 4000)
 
 export default app
--- server/src/io.js
+++ server/src/io.js
@@ -4,10 +4,19 @@ import http from 'http'
 import app from './app'
 
 const httpServer = http.createServer(app)
-const io = new SocketServer(httpServer)
+const io = new SocketServer(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+})
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
   console.log('a user connected')
+
+  socket.on('roomName', (data) => {
+    io.emit('newName', data)
+  })
 })
 
 export default httpServer
