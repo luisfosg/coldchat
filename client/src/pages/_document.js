d56d92cdd3b78c7df2ceca70a712f60d94ef3bea
LuisFOsG <luisfosgdev@gmail.com>
2021-12-07 22:28:26 -0500

    fix(Root): update dependencies

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
 
           <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet"/>
         </Head>
