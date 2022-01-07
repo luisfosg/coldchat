94b366c37d5f3d1c55859999afa97b7a8def2e7c
LuisFOsG <luisfosgdev@gmail.com>
2022-01-07 18:28:00 -0500

    fix(client): move nes.css to index.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,6 @@ import AppLayout from '@/layouts/app-layout'
 import { themeLight } from '@/styles/themes'
 import RouterPage from '@/Router'
 
-import 'normalize.css'
-import 'nes.css/css/nes.min.css'
 import '@/styles/globals.css'
 
 function App () {
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+
+import 'normalize.css'
+import 'nes.css/css/nes.min.css'
+
+import App from '@/App'
 
 ReactDOM.render(
   <React.StrictMode>
