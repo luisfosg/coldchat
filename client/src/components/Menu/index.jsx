a9b4677096aa8509d7f9ecf8270d8834ab7d66d9
LuisFOsG <luisfosgdev@gmail.com>
2022-01-20 19:20:41 -0500

    feat(client): add button toggle darkmode

--- client/src/components/Menu/index.jsx
+++ client/src/components/Menu/index.jsx
@@ -1,13 +1,23 @@
 import { useContext } from 'react'
+import clsx from 'clsx'
 
 import SocketContext from '@/context/SocketContext'
+
+import styles from './Menu.module.css'
+
+const buttonClass = clsx(styles.buttons, 'nes-btn')
+const buttonLogout = clsx(buttonClass, 'is-error')
+const buttonDarkmode = clsx(buttonClass, 'is-warning')
 
 const Menu = () => {
   const { logout } = useContext(SocketContext)
 
+  const handleDarkmode = () => {}
+
   return (
     <>
-      <button type="button" onClick={logout} className="nes-btn is-error">Cerrar Sesión</button>
+      <button type="button" onClick={logout} className={ buttonLogout }>Cerrar Sesión</button>
+      <button type="button" onClick={handleDarkmode} className={ buttonDarkmode }>Mode Dark</button>
     </>
   )
 }
