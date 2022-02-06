ebfa1d460bbf4d4bfaca378636b3c0a28178f541
LuisFOsG <luisfosgdev@gmail.com>
2022-02-05 22:37:37 -0500

    feat(client): add darkmode button in index.js

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,23 +1,36 @@
 import { useEffect, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import clsx from 'clsx'
+
+import useTheme from '@/hooks/useTheme'
 
 import SocketContext from '@/context/SocketContext'
 import FormUser from '@/components/FormUser'
 import Loading from '@/components/Loading'
 
+import { themeLight, themeDark } from '@/styles/themes'
+
+const buttonDarkmode = clsx('nes-btn', 'is-warning')
+
 export default function Home () {
   const { nickname, loading } = useContext(SocketContext)
+  const { theme, setTheme } = useTheme()
   const navigate = useNavigate()
 
   useEffect(() => {
     if (nickname && nickname !== '') navigate('/chat')
   }, [nickname])
 
+  const handleDarkmode = () => {
+    setTheme(theme.name === themeLight.name ? themeDark : themeLight)
+  }
+
   if (loading || (nickname && nickname !== '')) return <Loading />
   return (
     <>
       <h1>Bienvenido a ColdChat!</h1>
       <FormUser/>
+      <button type="button" onClick={handleDarkmode} className={ buttonDarkmode }>Mode Dark</button>
     </>
   )
 }
